23265c69e83a11785b8fa75f7aab23dcb91cd025
Kathryn Tsai <katalystkat@Kathryns-Air.attlocal.net>
2022-09-27 15:51:26 -0700

    webpack set up complete

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/, 
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+                },
+              },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /(node_modules)/,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            filename: './index.html',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: './client/stylesheets/styles.css'}],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './build'),
+        }
+    },
+    devtool: 'eval-source-map',
+}
